37e73cdf0272e5f193f1541c22bf0a28bc9fe754
NINJATSS <143434164+NINJATSS@users.noreply.github.com>
2023-09-29 10:30:50 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
           <NavLink className={styles.logo} to="/">
             <img alt="Gamba logo" src="/logo-2.svg" />
           </NavLink>
-        
+        <div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <div className={styles.externalLinks}>
             <a href="https://github.com/gamba-labs/gamba" target="_blank" rel="noreferrer">
