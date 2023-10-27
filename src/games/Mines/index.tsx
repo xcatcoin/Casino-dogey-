82ff143aadc1d2a99018a0ede1bc238b08bd26e7
xcatcoin <148262413+xcatcoin@users.noreply.github.com>
2023-10-27 20:33:55 +0100

    Update index.tsx

--- src/games/Mines/index.tsx
+++ src/games/Mines/index.tsx
@@ -4,8 +4,8 @@ import image from './logo.png'
 export default {
   name: 'Mines',
   short_name: 'mines',
-  description: `
-    There's money hidden beneath the squares. The reward will increase the more squares you reveal, but watch out for the 5 hidden mines. Touch one and you'll go broke. You can cash out at any time.
+  description:
+    creator:'ACoPzg7tNqhBXJm4JSQwKrJ7LiocezBTSZRQfLmrxpZz',
   `,
   theme_color: 'rgb(138 102 255)',
   image,
