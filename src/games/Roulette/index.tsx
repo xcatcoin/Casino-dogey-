b46f234a3f2a6982f723505c5b739858c8569f01
xcatcoin <148262413+xcatcoin@users.noreply.github.com>
2023-10-27 20:28:27 +0100

    Update index.tsx

--- src/games/Roulette/index.tsx
+++ src/games/Roulette/index.tsx
@@ -5,7 +5,7 @@ export default {
   name: 'Roulette',
   short_name: 'roulette',
   description: `
-    creator :ACoPzg7tNqhBXJm4JSQwKrJ7LiocezBTSZRQfLmrxpZz
+    creator : 'ACoPzg7tNqhBXJm4JSQwKrJ7LiocezBTSZRQfLmrxpZz',
   `,
   image,
   theme_color: 'rgb(29 232 126)',
